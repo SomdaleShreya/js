8ccee4e321aa295255736fd810c173ee22957aee
Shreya Somdale <129271007+SomdaleShreya@users.noreply.github.com>
2025-02-04 07:38:45 +0000

    DataTypes

---
+++ 01_basics/DataType.js
@@ -0,0 +1,9 @@
+let myName = "Shreyu"; //String
+let myAge = 21; //Number
+let isSingle = true; //Boolean
+let myBF = null; //Null
+let myFamily = undefined; //Undefined
+
+console.log(myName);
+console.log(myAge);
+console.table([myName,myAge,isSingle,myBF,myFamily])
